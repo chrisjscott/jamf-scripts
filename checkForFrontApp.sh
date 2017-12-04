3100e9d463b20477a2a004273c1eda894fa60370
Christopher Scott <chrisjscott@users.noreply.github.com>
2017-12-04 18:09:23 -0500

    Update checkForFrontApp.sh
    Incorporated rudimentary check for "Do Not Disturb" being turned on.

--- checkForFrontApp.sh
+++ checkForFrontApp.sh
@@ -8,11 +8,14 @@
 # presentation tools and then insert code to be executed in the 'else'
 
 currentUser=$(stat -f %Su /dev/console)
+# The following lin determines if "Do Not Disturb" is turned on; 0=Off, 1=On
+# Note that this isn't foolproof; if user has DND set up on a schedule then this won't detect when it's on as per the schedule
+notificationStatus=$(sudo -u $currentUser defaults read /Users/$currentUser/Library/Preferences/ByHost/com.apple.notificationcenterui.plist doNotDisturb)
 frontApp=$(launchctl asuser $(id -u $currentUser) osascript -e 'get POSIX path of (path to frontmost application)')
 
 presentationApps="Adobe Acrobat|Google Chrome.app|Keynote.app|Microsoft Powerpoint.app|Preview.app|Sketch.app"
 
-if [[ $frontApp =~ $presentationApps ]];
+if [[ $frontApp =~ $presentationApps ]] || [[ "$notificationStatus" = "1" ]];
 then
    true # user could be presenting; skip process and revisit later
 else
